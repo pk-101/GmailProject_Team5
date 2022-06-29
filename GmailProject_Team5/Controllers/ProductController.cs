2e6ac3f4d9c22efd4672c50780b5607fcdb9016b
Pankaj Kumar <1000071506@hexaware.com>
2022-06-29 12:48:46 +0530

    New view Added

---
+++ GmailProject_Team5/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GmailProject_Team5.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
